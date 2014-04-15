f5630d367a27482c50e298e34cb91b3ceef0b168
mikehobson <enquiries@metoffice.gov.uk>
2014-04-15 08:31:47 +0000

    ticket:28 for Matthew Hambley. Further additions to the support for the Gource visualisation tool. Custom merge into /Dynamo/trunk: r1222 cf. r1206 of /Dynamo/branches/dev/mhambley/r1128_Visualisation@1223
    git-svn-id: https://metomi/svn/lfric.xm/Dynamo/trunk@1224 4bbb03d4-c1cc-11e1-9ca4-71d347b01581

--- Docs/tools/doctools/__init__.py
+++ Docs/tools/doctools/__init__.py
@@ -1,28 +1,8 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 ##############################################################################
 # (c) The copyright relating to this work is owned jointly by the Crown,
 # Met Office and NERC 2014. However, it has been created with the help of the
 # GungHo Consortium, whose members are identified at
 # https://puma.nerc.ac.uk/trac/GungHo/wiki
 ##############################################################################
-
-export COMPILER?=gfortran
-.PHONY: test
-test: all
-	$(MAKE) -C src/test
-
-.PHONY: all
-all:
-	$(MAKE) -C src/dynamo
-
-.PHONY: run
-run: test
-	$(MAKE) -C run
-
-.PHONY: doc docs
-doc docs:
-	$(MAKE) -C Docs
-
-.PHONY: clean
-clean:
-	$(MAKE) -C src/dynamo clean
-	$(MAKE) -C src/test clean
